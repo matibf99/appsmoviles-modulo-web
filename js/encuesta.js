b48ce03c6591a45a744ad1f1a4d0512ebf7eeffa
Chaza07 <Chazita9@gmail.com>
2021-09-20 22:15:36 -0300

    incisos a y b del 4

--- js/encuesta.js
+++ js/encuesta.js
@@ -96,18 +96,19 @@ email.addEventListener("input", (e) => {
 let btnEnviar = document.getElementById("button-send");
 btnEnviar.addEventListener("click", enviar);
 
-
 function enviar(sel){
+    
     alert("Nombre: "+ nombre.value+ "\n" +
     "Apellido: "+ apellido.value + "\n"+
-    "Fecha de nacimiento: "+ fechaNacimiento.value + "\n"+
+    "Fecha de nacimiento: "+ fechaNacimiento.value + "\n"+ 
     "Sexo: " + sexo.options[sexo.selectedIndex].text + "\n"+
-    "Valoración: "+ valoracion.options[valoracion.selectedIndex].text + "\n"+
+    "Valoracion: "+ valoracion.options[valoracion.selectedIndex].text + "\n"+
     "Email: "+email.value + "\n"+
     "Comentario: "+comentario.value);
+    
 }
 
-// Restablecer valores
+//Restablecer valores
 let btnRestart = document.getElementById("btn-restablecer");
 btnRestart.addEventListener("click", restablecer);
 
@@ -121,7 +122,7 @@ function restablecer(){
     const comentario = document.getElementById("comentario").value= "";
 }
 
-// Cancelar
+//Cancelar
 const open = document.getElementById("btn-cancelar");
 const modal_container = document.getElementById("modal_conteiner");
 const btnNo = document.getElementById("btn-no");
@@ -129,6 +130,7 @@ const btnPaginaAnterior = document.getElementById("btn-paginaAnterior");
 
 open.addEventListener('click', () =>{
     modal_container.classList.add('show');
+    
 });
 
 btnNo.addEventListener('click', () =>{
